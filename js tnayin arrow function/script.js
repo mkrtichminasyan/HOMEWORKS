2a151af70f8c7e032d8dd1dd75160dd300192aaf
mkrtichminasyan <m-minasyan92@mail.ru>
2021-07-26 00:12:13 +0400

    homework arrow function

---
+++ js tnayin arrow function/script.js
@@ -0,0 +1,22 @@
+// var num = [2, 5, 100];
+// var str = num.map(i => i + "")
+// console.log(str);
+
+
+
+// var arr = [1,2,8,9]
+// // arr1 = arr.map((value) => {return value * value})
+// arr1 = arr.map(value => value * value)
+// console.log(arr1);
+
+
+
+// var names = ['Karen', 'Hayk', 'Sona', 'Lilit', 'Mkrtich']
+// var filterName = names.filter(v => v.length>=5)
+// console.log(filterName);
+
+
+
+// var num = [1, 3, 56, 87, 6, 0.6, 64, 3, -536]
+// var filterNum = num.filter(v => v > 4)
+// console.log(filterNum);
