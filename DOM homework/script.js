7c25ebd6fc769997cbd2ae5e2fc7548a158d2933
mkrtichminasyan <m-minasyan92@mail.ru>
2021-08-16 17:30:13 +0400

    homework

---
+++ DOM homework/script.js
@@ -0,0 +1,84 @@
+// let table = document.getElementById("axyusak")
+
+// let result = ""
+// for(var i = 1; i <= 10; i++){
+//     result += "<tr></tr>"
+//     for(var j = 1; j <= 10; j++){
+//         result += "<td>" + i*j + "</td>"
+//     }
+// }
+
+// table.innerHTML = result
+
+
+// let table = document.getElementById("axyusak")
+// let x = function(){
+//    return Math.floor(Math.random()*256)
+// }
+// let result = ""
+// for(var i = 1; i <= 10; i++){
+//     result += "<tr></tr>"
+//     for(var j = 1; j <= 10; j++){
+//         if(i > 1 && i < 10 && j > 1 && j < 10){
+//             result += "<td></td>"
+//         }else{
+//             result += "<td style = 'background-color:rgb("+x()+", "+x()+", " +x()+ ")'></td>" 
+//         }        
+//     }
+// }
+// table.innerHTML = result
+
+
+// let table = document.getElementById("axyusak")
+// let x = function(){
+//    return Math.floor(Math.random()*256)
+// }
+// let result = ""
+// for(var i = 1; i <= 10; i++){
+//     result += "<tr></tr>"
+//     for(var j = 1; j <= 10; j++){
+//         if(i > 1 && i < 10 && j > 1 && j < 10){
+//             result += "<td style = 'background-color:rgb("+ x() +"," + x() + "," + x() +")'></td>"
+//         }else{
+//             result += "<td></td>"             
+//         }        
+//     }
+// }
+// table.innerHTML = result
+
+
+let one = document.getElementById("one")
+let btn = document.getElementById("btn")
+
+// one.addEventListener("mousedown", function(){
+//     btn.innerHTML = "mousedown"
+// }) 
+
+// one.addEventListener("click", function(){
+//     btn.innerHTML = "click"
+// }) 
+
+// one.addEventListener("mouseup", function(){
+//     btn.innerHTML = "mouseup"
+// }) 
+
+// one.addEventListener("contextmenu", function(e){
+//     e.preventDefault()
+//     btn.innerHTML = "contextmenu"
+// }) 
+
+// one.addEventListener("mousemove", function(e){
+//     btn.innerHTML = `${e.pageX}   ${e.pageY}`
+// })
+
+// one.addEventListener("copy", function(e){
+//     e.preventDefault()
+//     btn.innerHTML = "ERROR!!!!!"
+// })
+
+// one.addEventListener("click", function(e){
+//     btn.style.fontSize = "8px"
+//     btn.innerHTML = e.target
+//     one.style.backgroundColor = "aqua"
+//     this.style.color = "red"
+// })
